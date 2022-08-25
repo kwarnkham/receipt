4eb818d525b8390935d8b0dbfc26f670490b1297
SAI KWARN KHAM <kwarnkham.moon@gmail.com>
2022-08-25 20:24:24 +0630

    fix referencing to null id

--- src/boot/init.js
+++ src/boot/init.js
@@ -7,24 +7,27 @@ import { api } from "./axios";
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async (/* { app, router, ... } */) => {
   const userStore = useUserStore();
-  const knownUsersKey = userStore.getUser.id + "knownUsers";
-  const knownItemsKey = userStore.getUser.id + "knownItems";
-  let knownUsers = LocalStorage.getItem(knownUsersKey);
-  let knownItems = LocalStorage.getItem(knownItemsKey);
 
-  if (!knownUsers) {
-    knownUsers = await api({
-      method: "GET",
-      url: "customer/known",
-    }).then(({ data }) => data);
-    LocalStorage.set(knownUsersKey, knownUsers);
-  }
+  if (userStore.getUser) {
+    const knownUsersKey = userStore.getUser.id + "knownUsers";
+    const knownItemsKey = userStore.getUser.id + "knownItems";
+    let knownUsers = LocalStorage.getItem(knownUsersKey);
+    let knownItems = LocalStorage.getItem(knownItemsKey);
 
-  if (!knownItems) {
-    knownItems = await api({
-      method: "GET",
-      url: "item/known",
-    }).then(({ data }) => data);
-    LocalStorage.set(knownItemsKey, knownItems);
+    if (!knownUsers) {
+      knownUsers = await api({
+        method: "GET",
+        url: "customer/known",
+      }).then(({ data }) => data);
+      LocalStorage.set(knownUsersKey, knownUsers);
+    }
+
+    if (!knownItems) {
+      knownItems = await api({
+        method: "GET",
+        url: "item/known",
+      }).then(({ data }) => data);
+      LocalStorage.set(knownItemsKey, knownItems);
+    }
   }
 });
