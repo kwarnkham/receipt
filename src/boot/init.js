30a7f87043b6df142cb8434602a392ea28b236ea
SAI KWARN KHAM <kwarnkham.moon@gmail.com>
2022-09-08 18:14:28 +0630

    use version

--- src/boot/init.js
+++ src/boot/init.js
@@ -1,4 +1,4 @@
-import { LocalStorage } from "quasar";
+import { Dialog, LocalStorage } from "quasar";
 import { boot } from "quasar/wrappers";
 import { useUserStore } from "src/stores/user";
 import { api } from "./axios";
@@ -7,6 +7,28 @@ import { api } from "./axios";
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async (/* { app, router, ... } */) => {
   const userStore = useUserStore();
+
+  if (process.env.MODE == "capacitor") {
+    const appConfig = await api({
+      method: "GET",
+      url: "app-config",
+    }).then(({ data }) => data);
+    // console.log(appConfig.version, process.env.APP_VERSION);
+    if (appConfig.version != process.env.APP_VERSION && appConfig.version) {
+      Dialog.create({
+        title: "Please update the app",
+        message: `Current verison is ${process.env.APP_VERSION}. Latest version is ${appConfig.version}`,
+        persistent: true,
+        ok: {
+          label: "Update",
+        },
+      }).onOk(() => {
+        // https://spaces.madewithheart.tech/Receipt/apk/PhoneVoucherV1.0.7.apk
+        const url = `${process.env.ASSETS_URL}/apk/PhoneVoucherV${appConfig.version}.apk`;
+        window.open(url);
+      });
+    }
+  }
 
   if (userStore.getUser) {
     const knownUsersKey = userStore.getUser.id + "knownUsers";
